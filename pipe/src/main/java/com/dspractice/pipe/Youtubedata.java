95dc526eac2c586b8d50285e9882f2a630e6742f
kvpvishnu11 <kvpvishnu11@gmail.com>
2023-10-15 14:41:20 -0400

    Doing Youtube data test

---
+++ pipe/src/main/java/com/dspractice/pipe/Youtubedata.java
@@ -0,0 +1,171 @@
+package com.dspractice.pipe;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Youtubedata {
+    private static String nextPageToken = null; 
+
+    public static void main(String[] args)
+    {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        /* Defining my Database related URL and credentials - "youtube" is my db name */
+        
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/youtube";
+        String username = "postgres";
+        String password = "150030441@klU";
+
+        try {
+            Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            
+            // Scheduling the code to run every 10 seconds
+            scheduler.scheduleAtFixedRate(() -> {
+                try {
+                    fetchAndInsertYouTubeComments(conn);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, 0, 300, TimeUnit.SECONDS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Video ID fetch from database dynamically from "videos" table in DB.
+    
+    private static String fetchVideoIdFromDatabase(Connection conn) {
+        try {
+            String query = "SELECT videoId FROM videos ORDER BY id DESC LIMIT 1";
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getString("videoId");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+   
+    // Once we fetch the video ID, using the same id to fetch the youtube comments
+    
+    private static void fetchAndInsertYouTubeComments(Connection conn) {
+        try {
+            
+        	  String videoId = fetchVideoIdFromDatabase(conn);
+
+              if (videoId == null) {
+                  System.out.println("No videoId found in the videos table.");
+                  return;
+              }
+
+        	
+            // Defining the API related parameters below
+              
+            String apiKey = "AIzaSyCwXjwmK-p7eWrher4jnBUHtDVfR54yhq4"; // My Key
+            int maxResults = 100;
+            
+            // API URL Generation
+            String apiUrl = "https://www.googleapis.com/youtube/v3/commentThreads?key=" + apiKey
+                    + "&textFormat=plainText&part=snippet&videoId=" + videoId
+                    + "&maxResults=" + maxResults;
+
+          
+            if (nextPageToken != null) {
+                apiUrl += "&pageToken=" + nextPageToken;
+            }
+
+            // Using HTTP client to send the request
+            
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .build();
+            
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                // Parsing the JSON response
+                JSONObject jsonResponse = new JSONObject(response.body());
+                JSONArray comments = jsonResponse.getJSONArray("items");
+
+                // Iterating to get what we need
+                for (int i = 0; i < comments.length(); i++) {
+                    JSONObject comment = comments.getJSONObject(i);
+                    JSONObject snippet = comment.getJSONObject("snippet");
+                    String commentId = comment.getString("id");
+                    String commentText = snippet.getJSONObject("topLevelComment")
+                            .getJSONObject("snippet")
+                            .getString("textDisplay");
+                    String commentCreatedDateTime = snippet.getJSONObject("topLevelComment")
+                            .getJSONObject("snippet")
+                            .getString("publishedAt");
+
+                    // Change the format of the date to be compatible with MySQL DATETIME
+                    SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                    SimpleDateFormat outputDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String commentCreatedDateTimeFormatted = outputDateFormat.format(inputDateFormat.parse(commentCreatedDateTime));
+
+                    // Insert the comment into the database while avoiding duplicates
+                    insertCommentIntoDatabase(conn, commentId, commentText, commentCreatedDateTimeFormatted);
+                }
+
+                // Getting the next page token for pagination
+                nextPageToken = jsonResponse.optString("nextPageToken", null);
+                System.out.println("Insertion done");
+            } else {
+                // Error handling
+                System.out.println("Error Response Code: " + response.statusCode());
+                System.out.println("Error Response Body:");
+                System.out.println(response.body());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Inserting Comment into DB by checking if the comment id already exists in DB or not
+
+    private static void insertCommentIntoDatabase(Connection conn, String commentId, String commentText, String commentCreatedDateTime) {
+        try {
+            // Checking if the comment id already exists in DB first
+            String checkQuery = "SELECT * FROM comments WHERE comment_id = ?";
+            PreparedStatement checkStatement = conn.prepareStatement(checkQuery);
+            checkStatement.setString(1, commentId);
+            if (checkStatement.executeQuery().next()) {
+                // If a comment id exists, do nothing
+                return;
+            }
+
+            // Insert the comment into the database
+            String insertQuery = "INSERT INTO comments (comment_id, comment_text, comment_created_datetime) VALUES (?, ?, ?)";
+            PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
+            insertStatement.setString(1, commentId);
+            insertStatement.setString(2, commentText);
+            insertStatement.setString(3, commentCreatedDateTime);
+         
+
+            insertStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
