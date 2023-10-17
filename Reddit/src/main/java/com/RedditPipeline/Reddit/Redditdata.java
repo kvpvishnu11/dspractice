c7e27b3e9c66941a8e0f41f657fc03e57097e85a
kvpvishnu11 <kvpvishnu11@gmail.com>
2023-10-17 10:49:52 -0400

    add changes

--- Reddit/src/main/java/com/RedditPipeline/Reddit/Redditdata.java
+++ Reddit/src/main/java/com/RedditPipeline/Reddit/Redditdata.java
@@ -1,5 +1,4 @@
 package com.RedditPipeline.Reddit;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,22 +19,36 @@ import org.json.JSONObject;
 import java.util.Date;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Redditdata {
     public static void main(String[] args) {
         // Scheduling our code for every 5 minutes
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
-        // Defining our Reddit OAuth credentials 
+        // Defining our Reddit OAuth credentials
         String clientId = "qlQUzr9kQi2k12vy0C4RBA";
         String clientSecret = "ohKP0Ao2IPhZcqfc_E2TfBsPkXlbyg";
         String userAgent = "MyRedditApp/1.0 (by PuzzledBrother9059; kvpvishnu11@gmail.com)";
 
+        // Variables to store the access token
+        AtomicReference<String> accessToken = new AtomicReference<>(null);
+
         executorService.scheduleAtFixedRate(() -> {
             try {
+                // Obtain the access token if not already obtained
+                if (accessToken.get() == null) {
+                    String token = getRedditAccessToken(clientId, clientSecret, userAgent);
+                    if (token != null) {
+                        accessToken.set(token);
+                    } else {
+                        return; // Stop processing if access token retrieval fails
+                    }
+                }
+
                 // My DB parameters
-                String jdbcUrl = "jdbc:postgresql://localhost:5432/reddit";
-                String username = "postgres";
+                String jdbcUrl = "jdbc:mysql://localhost:3306/reddit";
+                String username = "root";
                 String password = "150030441@klU";
 
                 /* Establishing connections and preparing required SQL queries for our scenario */
@@ -55,7 +68,7 @@ public class Redditdata {
                 while (subredditResultSet.next()) {
                     String subredditName = subredditResultSet.getString("subreddit_name");
 
-                    String apiUrl = "https://oauth.reddit.com/r/" + subredditName + "/comments.json";
+                    String apiUrl = "https://oauth.reddit.com/r/" + subredditName + "/comments.json?limit=1";
 
                     String credentials = clientId + ":" + clientSecret;
 
@@ -65,8 +78,7 @@ public class Redditdata {
                     String requestUrl = apiUrl;
                     HttpRequest request = HttpRequest.newBuilder()
                             .uri(URI.create(requestUrl))
-                            .header("Authorization", "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8)))
-                            .header("User-Agent", userAgent)
+                            .header("Authorization", "Bearer " + accessToken.get())
                             .header("User-Agent", userAgent)
                             .GET()
                             .build();
@@ -74,10 +86,22 @@ public class Redditdata {
                     // Capturing the response from the API
                     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+                    // Check for HTTP response code
+                    int statusCode = response.statusCode();
+                    if (statusCode != 200) {
+                        System.out.println("HTTP Request Error: " + statusCode);
+                        continue; // Skip processing if there's an HTTP error
+                    }
+
                     // Parsing the JSON response
                     JSONObject jsonResponse = new JSONObject(response.body());
-
-                    System.out.println("Reddit comment insertion done");
+                    System.out.println(jsonResponse);
+                    // Check for Reddit API errors
+                    if (jsonResponse.has("error")) {
+                        String errorMessage = jsonResponse.getString("error");
+                        System.out.println("Reddit API Error: " + errorMessage);
+                        continue; // Skip processing if there's a Reddit API error
+                    }
 
                     // Extract the comments data
                     if (jsonResponse.has("data")) {
@@ -122,5 +146,41 @@ public class Redditdata {
                 e.printStackTrace();
             }
         }, 0, 300, TimeUnit.SECONDS);
+    }
+
+    // Create a method to obtain the access token
+    private static String getRedditAccessToken(String clientId, String clientSecret, String userAgent) {
+        try {
+            // Construct the URL for obtaining an access token
+            String authUrl = "https://www.reddit.com/api/v1/access_token";
+
+            // Define the request body
+            String requestBody = "grant_type=client_credentials";
+
+            // Create an HTTP client
+            HttpClient client = HttpClient.newHttpClient();
+
+            // Create an HTTP request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(authUrl))
+                    .header("User-Agent", userAgent)
+                    .header("Authorization", "Basic " +
+                            Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes(StandardCharsets.UTF_8)))
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+
+            // Send the request and parse the JSON response to get an access token
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                JSONObject json = new JSONObject(response.body());
+                return json.getString("access_token");
+            } else {
+                System.err.println("Failed to obtain access token. HTTP Status Code: " + response.statusCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
